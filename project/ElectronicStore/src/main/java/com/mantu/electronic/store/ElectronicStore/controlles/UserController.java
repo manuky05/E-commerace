ebb4d2aa92eb6c469d88bb55331f9489be5bb4a3
manuky04 <manuky04@gmail.com>
2023-04-08 17:56:14 +0530

    UserController dome

--- project/ElectronicStore/src/main/java/com/mantu/electronic/store/ElectronicStore/controlles/UserController.java
+++ project/ElectronicStore/src/main/java/com/mantu/electronic/store/ElectronicStore/controlles/UserController.java
@@ -39,18 +39,20 @@ public class UserController {
     @Value("${user.profile.image.path}")
     private String imageUploadPath;
 
-    public UserController() {
-    }
+
 
     @PostMapping("/")
     public ResponseEntity<UserDto>createUser(@Valid @RequestBody UserDto userDto){
+        logger.info("Initiating Request for create User:"+userDto);
         UserDto userDto1 = this.userService.createUSer(userDto);
+        logger.info("User create successful:"+userDto1);
         return new ResponseEntity<UserDto>(userDto1, HttpStatus.CREATED);
     }
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto>updateUser(@PathVariable("userId") String userId
-            ,@Valid @RequestBody UserDto userDto){
+    public ResponseEntity<UserDto>updateUser(@PathVariable("userId") String userI,@Valid @RequestBody UserDto userDto){
+        logger.info("Initiating Request for updateUser:");
       UserDto updateduserDto =  this.userService.UpdateUser(userDto,userId);
+        logger.info("User Updated successful:"+updateduserDto);
       return new ResponseEntity<>(updateduserDto,HttpStatus.OK);
 
     }
@@ -61,36 +63,34 @@ public class UserController {
             @RequestParam(value = "pageSize",defaultValue="10",required = false) int pageSize,
             @RequestParam(value = "sortBy",defaultValue="name",required = false)String sortBy,
             @RequestParam(value = "sortDir",defaultValue="asc",required = false) String sortDir){
-        List<UserDto> allUser = this.userService.getAllUser(pageNumber,pageSize,sortBy,sortDir);
-
-        return new ResponseEntity<PageableResponse<UserDto>>((PageableResponse<UserDto>) allUser,HttpStatus.OK);
+        logger.info("Initiating Request for GetallUser:");
+        PageableResponse<UserDto> allUser =  this.userService.getAllUser(pageNumber,pageSize,sortBy,sortDir);
+        logger.info(" AllUser get  successful:"+ allUser);
+        return new ResponseEntity<PageableResponse<UserDto>>(allUser,HttpStatus.OK);
 //        return new ResponseEntity<PageableResponse<UserDto>>((PageableResponse<UserDto>) userService.getAllUser(pageNumber,pageSize,sortBy,sortDir),HttpStatus.OK);
 
     }
-    @DeleteMapping("/{userid}")
-//    public ResponseEntity<?>DeleteUser(@PathVariable("/userid") String userId){
+    @DeleteMapping("/{userId}")
+//    public ResponseEntity<?>DeleteUser(@PathVariable("/userId") String userId){
 //        userService.deleteUser(userId);
 //                return new ResponseEntity(Map.of("Message","User Delete Successful"),HttpStatus.OK);
-  public ResponseEntity<ApiResponseMessage>DeleteUser(@PathVariable("/userid") String userId){
+  public ResponseEntity<ApiResponseMessage>DeleteUser(@PathVariable String userId){
             userService.deleteUser(userId);
         ApiResponseMessage
                 message=ApiResponseMessage
                 .builder()
-                .message("User Delete Successfu")
+                .message("User Delete Successful")
                 .success(true)
                 .status(HttpStatus.OK)
                 .build();
+        logger.info("User Delete  successful:"+userId);
         return new ResponseEntity<>(message,HttpStatus.OK);
 
 
 
 
     }
-//    @GetMapping("/{userId}")
-//    public ResponseEntity<UserDto>findUser(@PathVariable String userId){
-//        return new ResponseEntity<>(userService.findUserById(userId),HttpStatus.OK);
-//
-//    }
+
     @GetMapping("/email/{email}")
     public ResponseEntity<UserDto>getUserByEmail(@PathVariable String email){
        return new ResponseEntity<>(userService.getUSerByEmail(email),HttpStatus.OK) ;
@@ -104,34 +104,36 @@ public class UserController {
     @GetMapping("/{userId}")
     public ResponseEntity<UserDto>findUserById(@Valid  @PathVariable String userId){
 
-//        UserDto userById = this.userService.findUserById(userId);
-//        return new ResponseEntity<UserDto>(userById,HttpStatus.OK);
+
         return  ResponseEntity.ok(this.userService.findUserById(userId));
 
     }
     //upload user image
     @PostMapping("/image/{userId}")
     public ResponseEntity<ImageResponse>uploadUserImage( @RequestParam("userImage")MultipartFile image,@PathVariable String userId) throws IOException {
-
-      String imageName= fileService.uploadFile(image,imageUploadPath);
+        logger.info("Initiating Request for uploadUserImage:"+userId);
+        String imageName= fileService.uploadFile(image,imageUploadPath);
         UserDto user = userService.findUserById(userId);
         user.setImageName(imageName);
         UserDto userDto = userService.UpdateUser(user, userId);
-        ImageResponse imageResponse=ImageResponse.builder().imageName(imageName).success(true).status(HttpStatus.CREATED).build();
-      return  new ResponseEntity<>(imageResponse,HttpStatus.CREATED);
+        ImageResponse imageResponse=ImageResponse.builder().imageName(imageName).success(true).message("Image upload successful").status(HttpStatus.CREATED).build();
+        logger.info(" uploadUserImage uploadSuccessful:"+imageResponse);
+        return  new ResponseEntity<>(imageResponse,HttpStatus.CREATED);
 
     }
 
     //serve user image
-    @GetMapping("/image/{userId}")
-    public void searchImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+    @GetMapping(value="/image/{userId}")
+    public void serveUserImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
         UserDto user = userService.findUserById(userId);
+
         logger.info("User image name:{}",user.getImageName());
         InputStream resource = fileService.getResource(imageUploadPath, user.getImageName());
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         StreamUtils.copy(resource,response.getOutputStream());
 
     }
+
 
 
 }
